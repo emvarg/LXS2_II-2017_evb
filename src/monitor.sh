1958d3cc25d2e0e7240d66dd26598ea7de689b35
Emmanuel Vargas B <emvargcr@gmail.com>
2017-03-18 13:46:51 -0600

    Creacion del script de monitoreo

---
+++ src/monitor.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Programa para realizar respaldos de documentacion
+
+# Ambos comandos son equivalentes
+current_pwd=`pwd`
+current_pwd=$(pwd)
+
+
+cd /usr/share/doc
+echo Reporte: >> /tmp/reporte
+date >> /tmp/reporte
+pwd >> /tmp/reporte
+du -sh .  >> /tmp/reporte
+
+echo Path de ejecucion `pwd`
+
+cd $current_pwd
